2e6773b739648cf424d8778369cd5c86e6919ced
Tosif Bhatti <>
2023-04-17 15:12:55 +0530

    -- Do Practice on LinearLayout (17/04/2023)

--- app/src/main/java/com/example/android_practice/MainActivity.java
+++ app/src/main/java/com/example/android_practice/MainActivity.java
@@ -7,17 +7,9 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText userName,email,nwePass,confirmPass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        userName=findViewById(R.id.username_et);
-        email=findViewById(R.id.email_et);
-
-
-
-
     }
 }
