d7b796a8e24a0db822d8ca9c830e0ff4b5054225
Tosif Bhatti <>
2023-05-09 19:02:32 +0530

    -- Did Practice on DataBinding, MultipleViewHolder, Parking Past reservation screen, custom alert box, etc. (09/05/2023)

---
+++ app/src/main/java/com/example/android_practice/adapter/RentalAdapter.kt
@@ -0,0 +1,25 @@
+package com.example.android_practice.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android_practice.databinding.PastReservationItemBinding
+import com.example.android_practice.screens.tagB.RentalViewHolder
+import com.example.android_practice.screens.tagB.dataModel.RentalModel
+
+class RentalAdapter(var rentalData: ArrayList<RentalModel>): RecyclerView.Adapter<RentalViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RentalViewHolder {
+        return RentalViewHolder(PastReservationItemBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+    }
+
+    override fun getItemCount(): Int {
+        return rentalData.count()
+    }
+
+    override fun onBindViewHolder(holder: RentalViewHolder, position: Int) {
+
+        holder.bind(rentalData[position], position)
+
+    }
+}
---
+++ app/src/main/java/com/example/android_practice/screens/tagB/dataModel/RentalModel.kt
@@ -0,0 +1,9 @@
+package com.example.android_practice.screens.tagB.dataModel
+
+
+data class RentalModel(val rentalID: String, val facility: String, val ReservationType: ReservationType, val entryDate: String, val ExitDate: String, val price: Int, val entryTime: String, val exitTime: String, val rating: Double?, val isRented: Boolean)
+
+
+
+
+
